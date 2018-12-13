3a89d654ec2974b47a4a782765871b9a6d6f3a49
Abed Murad <abdallah.Murad@mail.com>
2018-12-13 04:00:12 +0100

    Finish Replacing ListView With RecyclerView

--- app/src/main/java/com/udacity/sandwichclub/utils/CONST.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/CONST.java
@@ -12,7 +12,7 @@ public class CONST {
     public static String IMAGE_MEDIANOCHE = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Sandwich_de_Medianoche.jpg/800px-Sandwich_de_Medianoche.jpg";
     public static String IMAGE_PLJESKAVICE = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Pljeskavica_%286883073017%29.jpg/800px-Pljeskavica_%286883073017%29.jpg";
     public static String IMAGE_ROUJIAMO = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Roujiamo.jpg/800px-Roujiamo.jpg";
-    public static String IMAGE_SHAWARMA = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Shawarmafood.jpg/800px-Shawarmafood.jpg";
+    public static String IMAGE_SHAWARMA = "https://d2814mmsvlryp1.cloudfront.net/wp-content/uploads/Chicken-Shawarma-Stuffed-Pita.jpg";
     public static String IMAGE_VADA_PAV = "https://upload.wikimedia.org/wikipedia/commons/1/15/Vada_Paav-The_Mumbai_Burger.jpg";
 
     private static boolean mIsFirstTime = true;
