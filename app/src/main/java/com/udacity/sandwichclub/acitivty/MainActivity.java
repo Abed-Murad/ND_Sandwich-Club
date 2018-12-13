aece586a3b6ed9cfa9c4d1ca76e88aa5c62f4fb9
Abed Murad <abdallah.Murad@mail.com>
2018-12-13 03:43:28 +0100

    Working On the new RecyclerView

--- app/src/main/java/com/udacity/sandwichclub/acitivty/MainActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/acitivty/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.sandwichclub.acitivty;
 
+import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -39,23 +41,27 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private class SandwichesAdapter extends RecyclerView.Adapter {
+    private class SandwichesAdapter extends RecyclerView.Adapter<SandwichesAdapter.ViewHolder> {
 
+        private LayoutInflater mInflater;
         private String[] mSandwichesNames;
 
         public SandwichesAdapter(String[] sandwichesNames) {
             this.mSandwichesNames = sandwichesNames;
+            this.mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
         }
 
         @NonNull
         @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, final int position) {
-            return null;
+        public SandwichesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, final int position) {
+            CardSandwichBinding cardBinding = CardSandwichBinding.inflate(mInflater, viewGroup, false);
+            return new ViewHolder(cardBinding);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-
+        public void onBindViewHolder(@NonNull SandwichesAdapter.ViewHolder viewHolder, int i) {
+            viewHolder.bindData(mSandwichesNames[i]);
         }
 
         @Override
@@ -63,14 +69,17 @@ public class MainActivity extends AppCompatActivity {
             return mSandwichesNames.length;
         }
 
-        private class ViewHolder extends RecyclerView.ViewHolder {
+        public class ViewHolder extends RecyclerView.ViewHolder {
             CardSandwichBinding mBinding;
 
-            public ViewHolder(CardSandwichBinding binding) {
-                super(binding.getRoot());
-                this.mBinding = binding;
+            public ViewHolder(CardSandwichBinding itemView) {
+                super(itemView.getRoot());
+                this.mBinding = itemView;
                 mBinding.getRoot().setOnClickListener(view -> launchDetailActivity(getAdapterPosition()));
+            }
 
+            private void bindData(String sandwichName) {
+                mBinding.sandwichNameTextView.setText(sandwichName);
             }
         }
     }
---
+++ app/src/main/java/com/udacity/sandwichclub/utils/CONST.java
@@ -0,0 +1,14 @@
+package com.udacity.sandwichclub.utils;
+
+public class CONST {
+    public static String IMAGE_MEET = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Grilled_ham_and_cheese_014.JPG/800px-Grilled_ham_and_cheese_014.JPG";
+    public static String IMAGE_BOSNA= "https://upload.wikimedia.org/wikipedia/commons/c/ca/Bosna_mit_2_Bratw%C3%BCrsten.jpg";
+    public static String IMAGE_CHIVITO = "https://upload.wikimedia.org/wikipedia/commons/4/48/Chivito1.jpg";
+    public static String IMAGE_CLUB_SANDWICH = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Club_sandwich.png/800px-Club_sandwich.png";
+    public static String IMAGE_GUA_BAO = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Steamed_Sandwich%2Ctaken_by_LeoAlmighty.jpg/600px-Steamed_Sandwich%2Ctaken_by_LeoAlmighty.jpg";
+    public static String IMAGE_MEDIANOCHE = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Sandwich_de_Medianoche.jpg/800px-Sandwich_de_Medianoche.jpg";
+    public static String IMAGE_PLJESKAVICE = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Pljeskavica_%286883073017%29.jpg/800px-Pljeskavica_%286883073017%29.jpg";
+    public static String IMAGE_ROUJIAMO = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Roujiamo.jpg/800px-Roujiamo.jpg";
+    public static String IMAGE_SHAWARMA = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Shawarmafood.jpg/800px-Shawarmafood.jpg";
+    public static String IMAGE_VADA_PAV = "https://upload.wikimedia.org/wikipedia/commons/1/15/Vada_Paav-The_Mumbai_Burger.jpg";
+}
